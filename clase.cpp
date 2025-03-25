f449aa4b33f465ad10e997d8e802cc58bff74ffc
EmmanuelLopz <A01666331@tec.mx>
2025-03-24 22:52:32 -0600

    Declaracion de metodos

--- clase.cpp
+++ clase.cpp
@@ -3,7 +3,22 @@ using namespace std;
 
 class Coche{
     public:
+    Coche(){
+        this->precio = 150000;
+        this->color = "gris";
+        this->marca = "Toyota";
+    }
     
+    void manejar(){
+        cout << "El auto está conduciendo en este momento" << endl;
+    }
+    void detener(){
+        cout << "El auto se ha detenido" << endl;
+    }
+
+    void setColor(string color){
+        this->color = color;
+    }
     private:
     int precio;
     string color;
