45093d30eeb92585265008bc64f514744b07e11a
EmmanuelLopz <A01666331@tec.mx>
2025-03-24 22:47:59 -0600

    Creacion de clase Coche con atributos

---
+++ clase.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class Coche{
+    public:
+    
+    private:
+    int precio;
+    string color;
+    string marca;
+};
+
+
+int main(){
+
+
+    return 0;
+}
